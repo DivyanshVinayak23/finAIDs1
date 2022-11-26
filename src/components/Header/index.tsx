b94a477cbde04ca001c99e346615090122ce17a5
DivyanshVinayak23 <divyansh.vinayak@gmail.com>
2022-11-26 07:41:27 +0530

    Changed Stuff

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,23 @@
 import logoImg from '../../assets/logo.png';
 import { Container, Content } from './styles';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-
+import { useNavigate } from "react-router-dom";
 interface HeaderProps {
     onOpenNewTransactionModal: () => void;
 }
 
+
+
 export function Header({onOpenNewTransactionModal}: HeaderProps) {
+
     return (
         <Container>
             <Content>
-                <img src={logoImg} alt="" />
+                <img src={logoImg} alt="finAIDs" />
                 <button type="button" onClick={onOpenNewTransactionModal}>
                     New Transaction
                 </button>
                 <button type="button" onClick={onOpenNewTransactionModal}>
-                    Sign In
+                Sign In
                 </button>
                 <button type="button" onClick={onOpenNewTransactionModal}>
                     Sign Up
